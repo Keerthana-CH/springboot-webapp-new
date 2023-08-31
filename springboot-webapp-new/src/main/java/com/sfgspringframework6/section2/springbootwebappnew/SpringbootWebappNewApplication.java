9c7c7ab553ed7bb918b47e772b0e67a9051e4ce0
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-08-31 08:36:14 -0700

    main application

---
+++ springboot-webapp-new/src/main/java/com/sfgspringframework6/section2/springbootwebappnew/SpringbootWebappNewApplication.java
@@ -0,0 +1,13 @@
+package com.sfgspringframework6.section2.springbootwebappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebappNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebappNewApplication.class, args);
+	}
+
+}
---
+++ springboot-webapp-new/src/test/java/com/sfgspringframework6/section2/springbootwebappnew/SpringbootWebappNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfgspringframework6.section2.springbootwebappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebappNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
